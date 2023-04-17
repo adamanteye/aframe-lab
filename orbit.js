32a0adbdb0591d6d045c62c034ec78106e86c14c
adamanteye <yzh_0@outlook.com>
2023-04-18 00:40:34 +0800

    test https

---
+++ orbit.js
@@ -0,0 +1,11 @@
+class Body {
+    constructor(name, radius)
+    {
+        this.name =name;
+        this.radius=radius;
+    }
+}
+
+let earth = new Body("earth",0.3);
+let moon =new Body("moon",0.1);
+let sun = new Body("sun",1);
